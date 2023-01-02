94b47ab7121c5350fc41dbc04ec63215335b6aba
Tarela4ka <111660934+Tarela4ka@users.noreply.github.com>
2023-01-02 15:37:41 +0300

    Update Mutation.lua

--- Mutation.lua
+++ Mutation.lua
@@ -6,10 +6,13 @@ rares.AbilityRate = tostring(Color3.fromRGB(184,165,237))
 rares.GatherAmount = tostring(Color3.fromRGB(175,255,134))
 rares.ConvertAmount = tostring(Color3.fromRGB(254,202,65))
 local W = game:GetService("Players").LocalPlayer.Honeycomb.Value
-
+local mama = tostring(W.Cells["C"..tonumber(_G.Coordinates.Right)..","..tonumber(_G.Coordinates.Up)].LevelPart.SurfaceGui.TextLabel.TextColor3)
 
 Event:InvokeServer(tonumber(_G.Coordinates.Right), tonumber(_G.Coordinates.Up), "Neonberry", 1, false)
-while tostring(W.Cells["C"..tonumber(_G.Coordinates.Right)..","..tonumber(_G.Coordinates.Up)].LevelPart.SurfaceGui.TextLabel.TextColor3) ~= rares[_G.RollForMutation] do
+
+                                            
+while ((mama ~= rares[_G.RollForMutation]) or (mama ~= rares["AbilityRate"])) do
     Event:InvokeServer(tonumber(_G.Coordinates.Right), tonumber(_G.Coordinates.Up), food, _G.Amount, false)
+    mama = tostring(W.Cells["C"..tonumber(_G.Coordinates.Right)..","..tonumber(_G.Coordinates.Up)].LevelPart.SurfaceGui.TextLabel.TextColor3)
     task.wait(.005)
 end  
